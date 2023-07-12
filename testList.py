2981dc1e85e5e4443cec25e060584a7580a382d6
Dhanushka Gunasinghe <dhanushkapg@gmail.com>
2023-07-12 10:05:44 +0530

    Add files via upload

---
+++ testList.py
@@ -0,0 +1,15 @@
+num={'one':1,'two':2}
+print(num)
+
+num['one']=4
+print(num)
+
+
+num2=(1,2,3,4)
+print(type(num2))
+print(num2[1])
+#num2[1]=5 #tuples are mutable
+print(num2)
+
+frequency = []
+print(len(frequency))
